e7f067b78449953cf7e65048501101e14ba09bdd
Matan <matanhakim@gmail.com>
2023-03-13 15:17:40 +0200

    Change the RTL charachter from /u202B to /u2067, and add documenntation.

--- R/str_heb.R
+++ R/str_heb.R
@@ -1,3 +1,13 @@
-str_heb <- function(x = NULL) {
-  paste0("\u202B", x)
+#' Add a Unicode RTL-Isolating character `\u2067` to a character vector
+#'
+#' @param string A character vector. The default is `NULL` in case there is a need to only use the Unicode character.
+#'
+#' @return A character vector.
+#' @export
+#'
+#' @examples
+#' x <- "סימני ניקוד בסוף המשפט."
+#' str_heb(x)
+str_heb <- function(string = NULL) {
+  paste0("\u2067", string)
 }
